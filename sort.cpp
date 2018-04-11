f2e9ff4345ed5cdc3a3da41cb4ed0ee9a0039851
Daedalus128 <Daedalus128@users.noreply.github.com>
2018-04-11 12:02:22 -0400

    Lab 7 update push

---
+++ sort.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <cstdlib>//for "exit()" on some systems
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+\fn linearSearch
+\brief Search data for the first occurrence of key
+\param [in] key The value being searched for
+\param [in] data The data set that will be searched
+\returns location of key if found or -1 if not found
+*/
+int linearSearch(auto data, auto key);//prototype
+
+int linearSearch (auto data, auto key)
+{
+  for (int x =0; x < data.size(); x++)
+  {
+	if (data[x] == key) //data located
+  {
+	return x;
+  }
+  }
+ return -1; // data not found  
+}
+void bubbleSort (auto& inputs);
+
+void bubbleSort (auto& inputs)
+{
+  for ( int x = 0; x < inputs.size(); x++)
+  {
+	for ( int y = 0; y < inputs.size() - x; y++)
+	{
+	  if (inputs[y] > inputs[ y + 1])
+	  {
+	    swap(inputs[y], inputs[y + 1]);
+	  }
+	}
+  }
+}
+
+int main()
+{
+  vector<string> inputs;
+  string search_key, input;
+  int result;
+
+
+
+   cout<<"Welcome to \"search it\". We first need some input data."<<endl;
+   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+
+   cin>>input;
+ 
+    while(input != "#")//read an unknown number of inputs from keyboard
+    {
+       inputs.push_back(input);
+       cin>>input;
+    }
+
+   cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
+
+  if(inputs.size() == 0)//no input
+  {
+      cout<<endl<<"No input received, quiting..."<<endl<<endl;
+       exit(1);//nothing to do but quit program
+  }
+ 
+   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+  cout<<"Enter a value to search for: ";
+
+
+   cin>>search_key;
+ 
+    while(search_key != "#")//perform searches until sentinel entered
+    {
+        result = linearSearch(inputs,search_key);
+
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key; 
+    }
+
+cout << "initially entered data order: " << endl;
+for (int x = 0; x < inputs.size(); x++)
+{
+cout << inputs[x] << endl;
+}
+
+bubbleSort (inputs);
+
+cout << "sorting check: " << endl;
+for (int x = 0; x < inputs.size(); x++)
+{
+cout << inputs[x] << endl;
+}
+
+   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
+
+
+ return 0;
+}
